05197700acfbfc801b7535f252273a0ce4dc767a
ottomicheletti <michelettiotto@gmail.com>
2022-04-06 15:21:40 -0300

    Requisito 3

--- src/pages/Configuracao.jsx
+++ src/pages/Configuracao.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Configuracao = () => {
   return (
-    <div>
-      <h1 data-testid="settings-title">PÁGINA DE CONFIGURAÇÃO</h1>
-    </div>
-   );
-}
+      <div>
+        <h1 data-testid="settings-title">
+          PÁGINA DE CONFIGURAÇÃO
+        </h1>
+      </div>
+  );
+};
 
 export default Configuracao;
