2302e815340de6e1b2ce5da09267ddd4d80768e2
ottomicheletti <michelettiotto@gmail.com>
2022-04-07 17:28:17 -0300

    Requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
-const Feedback = () => <div data-testid="feedback-text">TELA DE FEEDBACK</div>;
+const Feedback = () => (
+  <div data-testid="feedback-text">
+    <Header />
+  </div>
+);
 
 export default Feedback;
