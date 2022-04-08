9c8f2d3aa098f1c5d6cde99019a4a6b44541343b
ottomicheletti <michelettiotto@gmail.com>
2022-04-08 11:54:19 -0300

    Requisito 18

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './Header.css';
 
 const Header = () => {
   const { name, score, gravatarEmail } = useSelector((state) => state.player);
   return (
     <header>
       <img
-        data-testid="header-profile-picture"
-        src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
-        alt={ name }
+        data-testid='header-profile-picture'
+        src={`https://www.gravatar.com/avatar/${gravatarEmail}`}
+        alt={name}
       />
-      <p data-testid="header-player-name">{name}</p>
-      <p data-testid="header-score">{score}</p>
+      <div>
+        <p data-testid='header-player-name'>{name}</p>
+        <p data-testid='header-score'>{score}</p>
+      </div>
     </header>
   );
 };
